69ce5d9ad68e7cf474018242cfe933262c9662d0
Julio Wong <julioo.wong@gmail.com>
2019-11-12 10:33:18 -0500

    commit

---
+++ src/test/java/com/wong/CursoSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
